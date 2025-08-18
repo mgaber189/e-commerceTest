c207f033d65ef4fb9e34d5f235f3dc2a8b37e466
unknown <mg3845617@gmail.com>
2025-08-18 20:52:27 +0300

    fix to deploy in vercal v3

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+    baseURL: '/', // ensure correct base
+  },
+    vite: {
+    build: {
+      rollupOptions: {
+        external: ['/personalLogo.png'],
+      },
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/i18n"],
